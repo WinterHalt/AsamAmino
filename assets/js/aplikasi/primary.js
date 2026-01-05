c38b85a8ee3fc0088dbde854d24271459a7bf0b9
Kelp <kelpintech@gmail.com>
2026-01-05 13:43:31 +0700

    Info File
    * Selesai Data Model
    
    * Selesai Data Bagian SIMRS
    
    * Selesai Data Bagian User
    
    * Selesai Main File D 1
    
    * Selesai Main File D 2

---
+++ assets/js/aplikasi/primary.js
@@ -0,0 +1,59 @@
+// Content of Canvas
+const chartElement = document.getElementById('myDashboardChart');
+const ctx = chartElement.getContext('2d');
+
+// Retrieve Data
+const labelData = JSON.parse(chartElement.getAttribute('data-label'));
+const valueData = JSON.parse(chartElement.getAttribute('data-value'));
+
+// Define Chart & Set
+new Chart(ctx, {
+  // Type Plot
+  type: 'bar',
+  // Define Data on Variable & Color
+  data: {
+    labels: labelData,
+    datasets: [{
+      label: 'Jumlah Input',
+      data: valueData,
+      backgroundColor: [
+        'rgba(79, 129, 189, 0.2)',
+        'rgba(192, 80, 77, 0.2)',
+        'rgba(155, 187, 89, 0.2)'
+      ],
+      borderColor: [
+        'rgba(79, 129, 189, 1)',
+        'rgba(192, 80, 77, 1)',
+        'rgba(155, 187, 89, 1)',
+      ],
+      borderWidth: 2
+    }]
+  },
+  // Option
+  options: {
+    plugins: {
+      // Disable Plot Display
+      legend: {
+        display: false
+      }
+    },
+    // Manipulate Plot Scale
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 1
+        }
+      }
+    },
+    // Config Plot Layout
+    layout: {
+      padding: {
+        left: 20,
+        right: 20,
+        top: 20,
+        bottom: 20
+      }
+    }
+  }
+})
