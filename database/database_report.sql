188ad07c5efe9689bd969cecdfae7ef4c217781a
Kelp <kelpintech@gmail.com>
2026-01-30 09:10:23 +0700

    Bujang

--- database/database_report.sql
+++ database/database_report.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE VIEW join_permintaan_aplikasi AS
     s_pengusul.name AS pengusul,
     tma.pic,
     tma.status,
-    date(tma.tanggal_pengajuan) as tanggal_pengajuan,
+    tma.tanggal_pengajuan as tanggal,
     tma.judul,
     tma.deskripsi,
     tma.surat,
